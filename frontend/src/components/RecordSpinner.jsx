227a05a2f3f7405eedae5961e3896c9d189f6414
Miles Eng <miles.eng@duke.edu>
2023-10-24 23:43:14 -0400

    WORKING NETLIFY login and view top artist/songs

--- frontend/src/components/RecordSpinner.jsx
+++ frontend/src/components/RecordSpinner.jsx
@@ -4,7 +4,7 @@ import Record from '/images/cd-gunmetal-50.svg'
 const RecordSpinner = () => {
   return (
       <img src={Record} alt='record spinning slowly'
-        className='w-1/3 align-[-0.125em] animate-[spin_10s_linear_infinite] mx-auto border-8 border-gunmetal-100 rounded-full' />
+        className='w-1/4 align-[-0.125em] animate-[spin_10s_linear_infinite] mx-auto border-8 border-gunmetal-100 rounded-full' />
   )
 }
 
